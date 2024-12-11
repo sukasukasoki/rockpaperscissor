c9d4813e0cbe1fe96698a2a4f55f8885562592aa
Syauki <189542110+sukasukasoki@users.noreply.github.com>
2024-12-11 16:56:51 +0700

    completed playRound function

--- script.js
+++ script.js
@@ -2,22 +2,54 @@
 console.log("Hello, World!")
 
 //Making computerChoice
-function computerChoice(){
+function getComputerChoice(){
     let a = Math.random()
 
     let choice = (a>=0 && a<=0.33)?"rock":(a>0.33 && a<=0.66)?"paper":"scissor";
     return choice
 }
 // Checking function's running
-// console.log(computerChoice());
+// console.log(getComputerChoice());
 
 //Making humanChoice
 
-function humanChoice(){
+function getHumanChoice(){
     let humaninput = prompt("type in between rock paper or scissor :").toLowerCase()
 
     return humaninput
 }
 
 // Checking function's running
-// console.log(humanChoice())
+// console.log(getHumanChoice())
+
+//Declaring Score
+let humanScore = 0;
+let computerScore = 0;
+
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+
+console.log(humanChoice)
+console.log(computerChoice)
+
+function playRound(humanChoice,computerChoice){
+    
+    if(humanChoice===computerChoice){
+        console.log(`it's a Tie! you chose ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)}, and so does the Computer`);
+    }else if(
+            (humanChoice==="rock"&&computerChoice==="scissor")||
+            (humanChoice==="paper"&&computerChoice==="rock")||
+            (humanChoice==="scissor"&&computerChoice==="paper")
+            ){
+                console.log(`You Win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}`)
+                humanScore++;
+        }else{
+        console.log(`You Lose! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} beaten by ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}!`)
+        computerScore++;
+    }
+
+}
+
+console.log(playRound(humanChoice,computerChoice))
+console.log(computerScore)
+console.log(humanScore)
