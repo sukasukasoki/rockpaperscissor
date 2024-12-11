c7cff0fcc0aa6f30352ee6a173cc896e634d8d09
Syauki <189542110+sukasukasoki@users.noreply.github.com>
2024-12-11 18:35:16 +0700

    added HTML and a little bit CSS for the Web View

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function getComputerChoice(){
 //Making humanChoice
 
 function getHumanChoice(){
-    let humaninput = prompt("type in between rock paper or scissor :").toLowerCase()
+    let humaninput = prompt("type in between rock paper or scissor for 5 times :").toLowerCase()
 
     return humaninput
 }
@@ -25,6 +25,7 @@ function getHumanChoice(){
 //Declaring Score
 let humanScore = 0;
 let computerScore = 0;
+let roundmessage;
 
 //Single Round game
 // Checking for Single Round
@@ -34,6 +35,7 @@ function playRound(humanChoice,computerChoice){
     
     if(humanChoice===computerChoice){
         console.log(`it's a Tie! you chose ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)}, and so does the Computer`);
+        roundmessage=`it's a Tie! you chose ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)}, and so does the Computer`;
     }else if(
             (humanChoice==="rock"&&computerChoice==="scissor")||
             (humanChoice==="paper"&&computerChoice==="rock")||
@@ -41,11 +43,12 @@ function playRound(humanChoice,computerChoice){
             ){
                 console.log(`You Win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}`)
                 humanScore++;
+                roundmessage=`You Win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}`;
         }else{
         console.log(`You Lose! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} beaten by ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}!`)
         computerScore++;
+        roundmessage = `You Lose! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} beaten by ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}!`;
     }
-
 }
 //End-Check for Single Round
 // console.log(playRound(humanChoice,computerChoice))
@@ -53,23 +56,32 @@ function playRound(humanChoice,computerChoice){
 // console.log(humanScore)
 
 //Five Round Game
+let arraymessage=[];
 function playGame(){
     for(let round = 1;round<=5;round++){
         let humanChoice=getHumanChoice();
         let computerChoice=getComputerChoice();
-
         playRound(humanChoice,computerChoice);
+        
+        arraymessage.push(roundmessage);
+        document.getElementById(`game${round}`).innerHTML = `Game ${round}`;
+        document.getElementById(`htmlmessage${round}`).innerHTML = roundmessage;
     }
 
     if (humanScore > computerScore) {
-        console.log(`\nYou win the Game! Final score: You - ${humanScore}, Computer - ${computerScore}`);
+        console.log(`You win the Game! Final score: You - ${humanScore}, Computer - ${computerScore}`);
+        resultmessage = `You win the Game! Final score: You - ${humanScore}, Computer - ${computerScore}`;
       } else if (computerScore > humanScore) {
-        console.log(`\nYou lose the Game! Final score: You - ${humanScore}, Computer - ${computerScore}`);
+        console.log(`You lose the Game! Final score: You - ${humanScore}, Computer - ${computerScore}`);
+        resultmessage = `You lose the Game! Final score: You - ${humanScore}, Computer - ${computerScore}`
       } else {
-        console.log(`\nIt's a tie Game! Final score: You - ${humanScore}, Computer - ${computerScore}`);
+        console.log(`It's a tie Game! Final score: You - ${humanScore}, Computer - ${computerScore}`);
+        resultmessage = `It's a tie Game! Final score: You - ${humanScore}, Computer - ${computerScore}`;
       }
+      document.getElementById(`resultmessage`).innerHTML = resultmessage;
 }
 
 //Run the Game
 playGame()
+console.log(`Summary : \n ${arraymessage}`);
 //Finished!
