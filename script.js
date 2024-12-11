2186e7a1fedca04aa0ddc0ea5a2e0fe281a4114b
Syauki <189542110+sukasukasoki@users.noreply.github.com>
2024-12-11 16:30:58 +0700

    completed computerChoice function

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 // Making Sure JS is working
 console.log("Hello, World!")
 
+//Making computerChoice
+function computerChoice(){
+    let a = Math.random()
+
+    let choice = (a>=0 && a<=0.33)?"rock":(a>0.33 && a<=0.66)?"paper":"scissor";
+    return choice
+}
+// Checking functiin's running
+// console.log(computerChoice());
