88cd76d3434c37b2d2c926ddf8df0aac11326031
Syauki <189542110+sukasukasoki@users.noreply.github.com>
2024-12-11 16:34:10 +0700

    Completed humanChoice function

--- script.js
+++ script.js
@@ -8,5 +8,16 @@ function computerChoice(){
     let choice = (a>=0 && a<=0.33)?"rock":(a>0.33 && a<=0.66)?"paper":"scissor";
     return choice
 }
-// Checking functiin's running
+// Checking function's running
 // console.log(computerChoice());
+
+//Making humanChoice
+
+function humanChoice(){
+    let humaninput = prompt("type in between rock paper or scissor :").toLowerCase()
+
+    return humaninput
+}
+
+// Checking function's running
+// console.log(humanChoice())
